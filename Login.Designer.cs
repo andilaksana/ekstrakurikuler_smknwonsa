583b7488b6e94379fc5c7aec8ab65724681ca07f
Andi Laksana <andilaksana125@gmail.com>
2025-05-09 15:23:14 +0700

    P3-pembuatan login aplikasi

---
+++ Login.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Aplikasi_Ekstrakurikuler_SMKNWONSA
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            label4 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            System.ComponentModel.ISupportInitialize pictureBox11 = ((System.ComponentModel.ISupportInitialize)pictureBox1);
+            pictureBox11.BeginInit();
+            SuspendLayout();
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.Location = new Point(358, 419);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 20);
+            label4.TabIndex = 21;
+            label4.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(338, 319);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 20);
+            label3.TabIndex = 20;
+            label3.Text = "Usernama/Email";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Clarendon Blk BT", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(105, 521);
+            button1.Name = "button1";
+            button1.Size = new Size(476, 63);
+            button1.TabIndex = 19;
+            button1.Text = "L  O  G  I  N";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Cambria", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(220, 446);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(361, 39);
+            textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Cambria", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(220, 345);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(361, 39);
+            textBox1.TabIndex = 17;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(105, 419);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(80, 79);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 16;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(105, 319);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(80, 76);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 15;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Monotype Corsiva", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(168, 252);
+            label2.Name = "label2";
+            label2.Size = new Size(360, 37);
+            label2.TabIndex = 14;
+            label2.Text = "Silahkan Login Terlebih Dahulu";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 21F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(16, 185);
+            label1.Name = "label1";
+            label1.Size = new Size(673, 43);
+            label1.TabIndex = 13;
+            label1.Text = "APLIKASI EKSTRAKURIKULER SMKN WONOSALAM";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(278, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 157);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 12;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Turquoise;
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.Location = new Point(105, 590);
+            button2.Name = "button2";
+            button2.Size = new Size(182, 40);
+            button2.TabIndex = 22;
+            button2.Text = "Lupa Password !";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Turquoise;
+            ClientSize = new Size(708, 660);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Login";
+            Text = "Ekstrakurikuler SMKNWONSA";
+            Load += Login_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label4;
+        private Label label3;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button2;
+    }
+}
