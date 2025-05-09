e74cada5aa67248cbf26d7e544cec1a8a67ea361
Andi Laksana <andilaksana125@gmail.com>
2025-05-09 15:25:01 +0700

    P4-pebuatan home aplikasi ekstrakurikuler

---
+++ Home.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace Aplikasi_Ekstrakurikuler_SMKNWONSA
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            panel1 = new Panel();
+            label9 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label6 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label7 = new Label();
+            listBox1 = new ListBox();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            button6 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button7 = new Button();
+            dataGridView1 = new DataGridView();
+            button8 = new Button();
+            textBox6 = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LightCoral;
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-2, -2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1192, 73);
+            panel1.TabIndex = 0;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(366, 17);
+            label9.Name = "label9";
+            label9.Size = new Size(359, 35);
+            label9.TabIndex = 30;
+            label9.Text = "SMK Negeri Wonosalam";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Matura MT Script Capitals", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(14, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(360, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Aplikasi Ekstrakurikuler";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(23, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 23);
+            label2.TabIndex = 1;
+            label2.Text = "NIS";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label3.Location = new Point(23, 119);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Nama Siswa";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label4.Location = new Point(23, 156);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Kelas";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(183, 84);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(183, 155);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(198, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(183, 119);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(340, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(183, 193);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(198, 27);
+            textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label5.Location = new Point(23, 194);
+            label5.Name = "label5";
+            label5.Size = new Size(149, 23);
+            label5.TabIndex = 7;
+            label5.Text = "Tempat, Tgl Lahir";
+            label5.Click += label5_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(387, 194);
+            dateTimePicker1.MaxDate = new DateTime(2035, 1, 1, 0, 0, 0, 0);
+            dateTimePicker1.MinDate = new DateTime(2000, 1, 1, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(136, 27);
+            dateTimePicker1.TabIndex = 9;
+            dateTimePicker1.Value = new DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label6.Location = new Point(23, 227);
+            label6.Name = "label6";
+            label6.Size = new Size(119, 23);
+            label6.TabIndex = 10;
+            label6.Text = "Jenis Kelamin";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(183, 227);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(88, 24);
+            radioButton1.TabIndex = 11;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Laki-Laki";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(293, 227);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(104, 24);
+            radioButton2.TabIndex = 12;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Perempuan";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label7.Location = new Point(23, 262);
+            label7.Name = "label7";
+            label7.Size = new Size(71, 23);
+            label7.TabIndex = 13;
+            label7.Text = "Jurusan";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "[Pilihan Jurusan]", "Agribisnis Tanaman Perkebunan", "Kuliner", "Teknik Kendaraan Ringan Otomotif", "Teknik Pemesinan" });
+            listBox1.Location = new Point(183, 262);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(259, 24);
+            listBox1.TabIndex = 14;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(183, 301);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(340, 97);
+            textBox5.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label8.Location = new Point(23, 302);
+            label8.Name = "label8";
+            label8.Size = new Size(68, 23);
+            label8.TabIndex = 15;
+            label8.Text = "Alamat";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(566, 362);
+            button3.Name = "button3";
+            button3.Size = new Size(246, 36);
+            button3.TabIndex = 19;
+            button3.Text = "Browse Foto";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(566, 84);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(246, 261);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 20;
+            pictureBox1.TabStop = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaption;
+            button6.Location = new Point(834, 84);
+            button6.Name = "button6";
+            button6.Size = new Size(133, 36);
+            button6.TabIndex = 23;
+            button6.Text = "Clear";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Orange;
+            button4.Location = new Point(987, 133);
+            button4.Name = "button4";
+            button4.Size = new Size(133, 36);
+            button4.TabIndex = 24;
+            button4.Text = "Update";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Lime;
+            button5.Location = new Point(834, 133);
+            button5.Name = "button5";
+            button5.Size = new Size(133, 36);
+            button5.TabIndex = 25;
+            button5.Text = "Input";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Red;
+            button7.Location = new Point(987, 84);
+            button7.Name = "button7";
+            button7.Size = new Size(133, 36);
+            button7.TabIndex = 26;
+            button7.Text = "Delete";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(23, 459);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1097, 289);
+            dataGridView1.TabIndex = 27;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(874, 412);
+            button8.Name = "button8";
+            button8.Size = new Size(246, 36);
+            button8.TabIndex = 28;
+            button8.Text = "Search";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(23, 417);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(841, 27);
+            textBox6.TabIndex = 29;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
+            ClientSize = new Size(1152, 760);
+            Controls.Add(textBox6);
+            Controls.Add(button8);
+            Controls.Add(dataGridView1);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button6);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(textBox5);
+            Controls.Add(label8);
+            Controls.Add(listBox1);
+            Controls.Add(label7);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label6);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Name = "Home";
+            Text = "Ekstrakurikuler SMKNWONSA";
+            Load += Home_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private Label label6;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label7;
+        private ListBox listBox1;
+        private TextBox textBox5;
+        private Label label8;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private Button button6;
+        private Button button4;
+        private Button button5;
+        private Button button7;
+        private DataGridView dataGridView1;
+        private Button button8;
+        private TextBox textBox6;
+        private Label label9;
+    }
+}
